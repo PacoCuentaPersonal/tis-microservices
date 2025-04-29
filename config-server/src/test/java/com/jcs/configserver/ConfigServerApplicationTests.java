e069fad3d97009cb733467f42ab0424aed5c1913
JCS-MAIN_MACHINE <pacolucasmargarita@gmail.com>
2025-04-28 20:26:35 -0500

    Added configurations of microservices

---
+++ config-server/src/test/java/com/jcs/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcs.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
